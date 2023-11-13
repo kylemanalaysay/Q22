f939157c653e12f8393404d42b088e7c4f43a7b3
kylemanalaysay <kylemanalaysay77@gmail.com>
2023-11-13 05:08:14 +0000

    Committing Web App Files

---
+++ midterm_app.py
@@ -0,0 +1,21 @@
+# Add to this file for the sample app lab
+from flask import Flask
+from flask import request
+from flask import render_template
+
+sample = Flask(__name__)
+
+@sample.route("/")
+def home():
+    return render_template("index.html")
+
+@sample.route("/login.html")
+def login():
+    return render_template("login.html")
+
+@sample.route("/registration.html")
+def register():
+    return render_template("registration.html")
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=5000)
